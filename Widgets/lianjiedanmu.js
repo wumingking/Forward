263c9289f14fc356942f122698aecb83934f34a8
coisini114 <63687467+coisini114@users.noreply.github.com>
2025-08-10 07:21:43 +0800

    更新 lianjiedanmu.js

--- Widgets/lianjiedanmu.js
+++ Widgets/lianjiedanmu.js
@@ -15,7 +15,7 @@
 WidgetMetadata = {
   id: "lianjiedanmu",
   title: "播放链接弹幕",
-  version: "1.0.0",
+  version: "1.0.1",
   requiredVersion: "0.0.2",
   description: "从指定播放链接和服务器获取弹幕",
   author: "huangxd_",
